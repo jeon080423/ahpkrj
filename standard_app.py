d93c95803ba898e19c438256e354d7bd1d03afc8
jeon080423 <jeon080423@gmail.com>
2026-07-24 08:13:25 +0900

    Fix: Switch back to official streamlit-quill package with inline conditional render to permanently fix all Streamlit Cloud bugs

--- standard_app.py
+++ standard_app.py
@@ -9297,7 +9297,7 @@ with col_main:
             - Lead Researcher : [Enter Name]
             - Contact : [Enter Phone or Email]"""
     
-                from quill_editor import st_quill
+                from streamlit_quill import st_quill
                 
                 st.markdown(f"**{_('조사 목적 및 안내문', 'Survey Purpose & Instructions')}**")
                 
